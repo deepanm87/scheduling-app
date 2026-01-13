7b02e3e6ecf88c87887c0b16e2883c5c15ae8cb5
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2026-01-13 16:14:47 -0500

    setup sanity schema types for studio

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,18 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import type { SchemaTypeDefinition } from 'sanity'
+import { userType } from './userType'
+import { meetingType } from './meetingType'
+import { availabilitySlotType } from './availabilitySlotType'
+import { connectedAccountType } from './connectedAccountType'
+import { bookingType } from './bookingType'
+import { feedbackType } from './feedbackType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [
+    userType,
+    meetingType,
+    availabilitySlotType,
+    connectedAccountType,
+    bookingType,
+    feedbackType
+  ],
 }
